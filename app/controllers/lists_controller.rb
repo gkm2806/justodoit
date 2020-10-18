62c99bbbd2a755c418771e6c71e642735c1dc185
Kukiel <gabrielkukiel@gmail.com>
2020-10-18 18:46:54 -0300

    [UPDATE] Lists controllers methods for saving items on creation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,28 @@ class ListsController < ApplicationController
     @lists = List.where(shared: true)
   end
 
+  
+  def create
+    @list = current_user.lists.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+  def list_params
+    params.require(:list).permit(:name, :checked, :shared, items_attributes: [:content])
+  end
 
   def find_list
     @list = List.includes(items: :items).find(params[:id])
-  end
-
-  def list_params
-    params.require(:list).permit(:name, :checked, :shared)
   end
 end
--- app/controllers/user/lists_controller.rb
+++ app/controllers/user/lists_controller.rb
@@ -2,4 +2,9 @@ class User::ListsController < ApplicationController
   def index
     @lists = current_user.lists.all
   end
+
+  def new
+    @list = current_user.lists.new
+    3.times { @list.items.build }
+  end
 end

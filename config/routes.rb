c16c9290a1eea011f2787d31ce844228da5b8a65
Kukiel <gabrielkukiel@gmail.com>
2020-10-18 14:31:52 -0300

    [NEW] Show List and item controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :lists
+
+  resources :items do
+    collection do
+      post :toggle_checked
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

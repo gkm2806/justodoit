699f52641b0b91bab33c12123eb424fc37c06eaa
Kukiel <gabrielkukiel@gmail.com>
2020-10-18 22:59:17 -0300

    [NEW] User Favorite lists relation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,13 @@
 class List < ApplicationRecord
   belongs_to :user
   has_many :items, as: :itemable
+  has_many :user_favorited_lists
 
   accepts_nested_attributes_for :items,
     allow_destroy: true,
     reject_if: :all_blank
+
+  def favorites_count
+    user_favorited_lists.count
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :lists
+  has_many :user_favorited_lists
+  has_many :favorited_lists, through: :user_favorited_lists, source: :list
 end

3c5a22d0b08f1c408a38572be5fb6afb151a008f
Kukiel <gabrielkukiel@gmail.com>
2020-11-02 21:45:27 -0400

    [UPDATE] Moving notification business process to service

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,21 +16,10 @@ class ItemsController < ApplicationController
       response[:error]
     end
 
-    share_if_public
     redirect_to @item.itemable
   end
 
   private
-
-  def share_if_public
-    itemable = @item.itemable
-    if itemable.shared
-      itemable.user_favorited_lists.each do |user_favorited|
-        quote = "#{itemable.user.name} atualizou a lista \"#{itemable.name}\""
-        ActionCable.server.broadcast "notifications:#{user_favorited.user.id}", { html: quote }
-      end
-    end
-  end
 
   def find_itemable
     @itemable = item_params[:itemable_type].classify.constantize.find(item_params[:itemable_id])
